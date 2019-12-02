50c8bb61c12cf96dc8ba0f338989117a66465dbf
RobnettSean22 <robnettsean22@gmail.com>
2019-12-02 16:12:51 -0700

    b

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const 12 = {
-    day1: "",
-    day2: "",
-    day3: "",
-    day4: "",
-    day5: "",
-    day6: "",
-    day7: "",
-    day8: "",
-    day9: "",
-    day10: "",
-    day11: "",
-    day12: ""
-}
+const get12 = {
+  day1: "",
+  day2: "",
+  day3: "",
+  day4: "crack",
+  day5: "",
+  day6: "",
+  day7: "socks",
+  day8: "",
+  day9: "",
+  day10: "smoke",
+  day11: "",
+  day12: ""
+};
