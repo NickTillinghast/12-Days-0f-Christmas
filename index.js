5bcbbed2aff5383e92b34271d2dc9fea47962f07
charles <codelyfewithcharlie@gmail.com>
2019-12-02 16:18:40 -0700

    test

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
-<<<<<<< HEAD
 const get12 = {
-  day1: "",
-  day2: "",
-  day3: "",
-  day4: "crack",
-  day5: "",
-  day6: "",
-  day7: "socks",
-  day8: "",
-  day9: "",
-  day10: "smoke",
-  day11: "",
-  day12: ""
+  day1: "1",
+  day2: "2",
+  day3: "3",
+  day4: "4",
+  day5: "5",
+  day6: "6",
+  day7: "7",
+  day8: "8",
+  day9: "9",
+  day10: "10",
+  day11: "11",
+  day12: "12"
 };
-=======
-const 12 = {
-    day1: "1",
-    day2: "2",
-    day3: "party time",
-    day4: "4",
-    day5: "5",
-    day6: "almost the number of the beast",
-    day7: "7",
-    day8: "octotime",
-<<<<<<< HEAD
-    day9: "",
-    day10: "",
-    day11: "",
-    day12: ""
-}
->>>>>>> ca4b20049a77f7f5566de12f10ae2c50f5f78a37
-=======
-    day9: "9",
-    day10: "10",
-    day11: "11",
-    day12: "12"
-}
->>>>>>> 420f678f6194078d0f774fd610cc3000737353ce
